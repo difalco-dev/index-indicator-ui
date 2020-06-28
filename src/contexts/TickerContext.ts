1edf8afbdb9ef77351565ff97ea5ac38a0c121d6
Anthony DiFalco <difalco.dev@gmail.com>
2020-06-28 15:37:42 -0400

    ticker/setup: adding skeleton

--- src/contexts/TickerContext.ts
+++ src/contexts/TickerContext.ts
@@ -8,12 +8,27 @@ export const model_obj = [
         indicators: [
             {
                 ind_title: '1.11',
+                ind_val: 1.11,
             },
-        ],
-        links: [
             {
-                title: 'title1',
-                link: 'link1',
+                ind_title: '2.11',
+                ind_val: 2.11,
+            },
+            {
+                ind_title: '3.11',
+                ind_val: 3.11,
+            },
+            {
+                ind_title: '4.11',
+                ind_val: 4.11,
+            },
+            {
+                ind_title: '5.11',
+                ind_val: 5.11,
+            },
+            {
+                ind_title: '6.11',
+                ind_val: 6.11,
             },
         ],
     },
@@ -22,13 +37,148 @@ export const model_obj = [
         price: 2.22,
         indicators: [
             {
-                title: '2.22',
+                ind_title: '1.11',
+                ind_val: 1.11,
+            },
+            {
+                ind_title: '2.11',
+                ind_val: 2.11,
+            },
+            {
+                ind_title: '3.11',
+                ind_val: 3.11,
+            },
+            {
+                ind_title: '4.11',
+                ind_val: 4.11,
+            },
+            {
+                ind_title: '5.11',
+                ind_val: 5.11,
+            },
+            {
+                ind_title: '6.11',
+                ind_val: 6.11,
             },
         ],
-        links: [
+    },
+    {
+        ticker_id: 'EX3',
+        price: 3.22,
+        indicators: [
             {
-                title: 'title1',
-                link: 'link1',
+                ind_title: '1.11',
+                ind_val: 1.11,
+            },
+            {
+                ind_title: '2.11',
+                ind_val: 2.11,
+            },
+            {
+                ind_title: '3.11',
+                ind_val: 3.11,
+            },
+            {
+                ind_title: '4.11',
+                ind_val: 4.11,
+            },
+            {
+                ind_title: '5.11',
+                ind_val: 5.11,
+            },
+            {
+                ind_title: '6.11',
+                ind_val: 6.11,
+            },
+        ],
+    },
+    {
+        ticker_id: 'EX4',
+        price: 4.22,
+        indicators: [
+            {
+                ind_title: '1.11',
+                ind_val: 1.11,
+            },
+            {
+                ind_title: '2.11',
+                ind_val: 2.11,
+            },
+            {
+                ind_title: '3.11',
+                ind_val: 3.11,
+            },
+            {
+                ind_title: '4.11',
+                ind_val: 4.11,
+            },
+            {
+                ind_title: '5.11',
+                ind_val: 5.11,
+            },
+            {
+                ind_title: '6.11',
+                ind_val: 6.11,
+            },
+        ],
+    },
+    {
+        ticker_id: 'EX5',
+        price: 5.22,
+        indicators: [
+            {
+                ind_title: '1.11',
+                ind_val: 1.11,
+            },
+            {
+                ind_title: '2.11',
+                ind_val: 2.11,
+            },
+            {
+                ind_title: '3.11',
+                ind_val: 3.11,
+            },
+            {
+                ind_title: '4.11',
+                ind_val: 4.11,
+            },
+            {
+                ind_title: '5.11',
+                ind_val: 5.11,
+            },
+            {
+                ind_title: '6.11',
+                ind_val: 6.11,
+            },
+        ],
+    },
+    {
+        ticker_id: 'EX6',
+        price: 6.22,
+        indicators: [
+            {
+                ind_title: '1.11',
+                ind_val: 1.11,
+            },
+            {
+                ind_title: '2.11',
+                ind_val: 2.11,
+            },
+            {
+                ind_title: '3.11',
+                ind_val: 3.11,
+            },
+            {
+                ind_title: '4.11',
+                ind_val: 4.11,
+            },
+            {
+                ind_title: '5.11',
+                ind_val: 5.11,
+            },
+            {
+                ind_title: '6.11',
+                ind_val: 6.11,
             },
         ],
     },
